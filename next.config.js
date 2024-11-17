858062c6d124b6a9a9577c15d86934fb4411ab57
在中国的泰国青年_ <132539847+dekthaiinchina@users.noreply.github.com>
2024-11-17 23:45:48 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -53,7 +53,6 @@ const nextConfig = {
 module.exports = withBundleAnalyzer(nextConfig);
 
 // Injected content via Sentry wizard below
-
 const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(
@@ -61,7 +60,6 @@ module.exports = withSentryConfig(
   {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
-
     // Suppresses source map uploading logs during build
     silent: true,
     org: "stardewvalley-app",
@@ -70,7 +68,6 @@ module.exports = withSentryConfig(
   {
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
